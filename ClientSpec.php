88ffe55e9a758ab2fba36da14f69014cb95f7fe3
Koen Vinken <koen.vinken@wijs.be>
2016-10-01 15:00:18 +0200

    Configure spec setup.

---
+++ ClientSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\Marvel;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ClientSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaqsdfqveType('ikoene\Marvel\Client');
+    }
+}
---
+++ spec/Marvel/ClientSpec.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace spec\ikoene\Marvel;
+
+use ikoene\Marvel\Client;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ClientSpec extends ObjectBehavior
+{
+    function let() {
+        $this->beConstructedWith(
+            'your_public_api_key',
+            'your_private_api_key'
+        );
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Client::class);
+    }
+}
